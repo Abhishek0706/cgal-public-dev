1cfb97c6d0d211eaff4de9b73063bb556ce759c8
Abhishek0706 <apatil@cs.iitr.ac.in>
2020-02-02 20:33:32 +0530

    docs: Callbback.cpp

--- Arrangement_on_surface_2/demo/Arrangement_on_surface_2/Callback.cpp
+++ Arrangement_on_surface_2/demo/Arrangement_on_surface_2/Callback.cpp
@@ -28,11 +28,22 @@
 
 namespace CGAL {
 namespace Qt {
-
+//! Constructor of Callback
+/*!
+ * \param parent QObject
+ */
 Callback::Callback( QObject* parent ) : QObject( parent ) { }
 
+//! Function to reset the Callback
 void Callback::reset( ) { }
 
+//! Function to determine the event type of Callback
+/*!
+ * \brief Callback::eventFilter
+ * \param object QObject
+ * \param event QEvent
+ * \return
+ */
 bool Callback::eventFilter( QObject* object, QEvent* event )
 {
   if ( event->type( ) == QEvent::GraphicsSceneMouseMove )
@@ -61,14 +72,19 @@ bool Callback::eventFilter( QObject* object, QEvent* event )
   return QObject::eventFilter( object, event );
 }
 
-  void Callback::mousePressEvent(QGraphicsSceneMouseEvent* /* event */) { }
+//! Callback function for mouse press event
+void Callback::mousePressEvent(QGraphicsSceneMouseEvent* /* event */) { }
 
-  void Callback::mouseMoveEvent(QGraphicsSceneMouseEvent* /* event */) { }
+//! Callback function for mouse movement
+void Callback::mouseMoveEvent(QGraphicsSceneMouseEvent* /* event */) { }
 
-  void Callback::mouseReleaseEvent(QGraphicsSceneMouseEvent* /* event */) { }
+//! Callback Function for mouse button release
+void Callback::mouseReleaseEvent(QGraphicsSceneMouseEvent* /* event */) { }
 
-  void Callback::keyPressEvent(QKeyEvent* /* event */)  { }
+//! Callback Function for pressing a key
+void Callback::keyPressEvent(QKeyEvent* /* event */)  { }
 
+//! Callback function for slot model change
 void Callback::slotModelChanged( ) { }
 
 } // namespace Qt
